f24dee2ca0652763f860d9323ce2294011de8df8
wonchan-choi <95732091+wonchan-choi@users.noreply.github.com>
2025-05-21 20:52:07 +0000

    Deploying to gh-pages from @ wonchan-choi/wonchan-choi.github.io@662225aca4c521a46a93b7ffadd474c4b79a4bb1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "Funded projects",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
