3be250fc525891212b504ecfc18d1cc2e0d29620
wonchan-choi <95732091+wonchan-choi@users.noreply.github.com>
2025-07-31 16:52:06 +0000

    Deploying to gh-pages from @ wonchan-choi/wonchan-choi.github.io@583abe2f4fdd12193f6b54efe5d71aac54b91012 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -49,8 +49,8 @@ ninja.data = [{
           section: "News",},{id: "news-our-arist-chapter-investigating-the-interactions-between-individuals-with-disabilities-and-information-retrieval-systems-a-review-of-help-seeking-situations-search-tactics-and-design-recommendations-has-been-accepted-for-publication-in-jasist",
           title: 'Our ARIST chapter, “Investigating the Interactions between Individuals with Disabilities and Information Retrieval...',
           description: "",
-          section: "News",},{id: "news-our-paper-exploring-applications-and-user-experience-with-generative-ai-tools-a-content-analysis-of-reddit-posts-on-chatgpt-quot-ranked-among-the-top-10-of-most-viewed-papers-in-2023-proceedings-of-asist",
-          title: 'Our paper, “Exploring Applications and User Experience with Generative AI Tools--A Content Analysis...',
+          section: "News",},{id: "news-our-short-paper-ranked-among-the-top-10-of-most-viewed-papers-in-2023-proceedings-of-asis-amp-t",
+          title: 'Our short paper ranked among the top 10% of most-viewed papers in (2023...',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2025-04-16/";
